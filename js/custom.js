9c3b5ed731d534391edab30c7f62e199789f283d
Bijan1432 <91670516+Bijan1432@users.noreply.github.com>
2022-11-12 16:56:51 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -68,3 +68,10 @@ $('.r-m').click(function () {
   });
 
 });
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarsExample04')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
